971535f399608dd54a6fe14bfdfbb568f4e5e892
Balaji Ramesh <30927538+balaji025@users.noreply.github.com>
2025-08-24 22:00:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,4 +14,6 @@ def encrypt():
     return jsonify({'encrypted': encrypted_data})  # Send the result back as JSON
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 4000))
+    app.run(host="0.0.0.0", port=port)
+
